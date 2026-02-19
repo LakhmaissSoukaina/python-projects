5a995f23ceeb225bde711f23dad877429f638aa0
soukaina <soukainalakhmaiss743@gmail.com>
2026-02-19 10:28:13 +0000

    using while loop

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 print('hello, world!')
-x=1
+x=7
 print(x)
 x=x+1
 print(x)
+
+
 if x<10:
     print("smaller")
 if x>20:
     print("bigger")
 print("finis!")
+
+while x>0:
+    print(x)
+    x=x-1
+print("end of the loop")
